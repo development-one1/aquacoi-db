0d53c3659eb23bf201dbeefc7307c67033bd84e0
Felipe Develop <camachorodriguezandresfelipe@gmail.com>
2025-05-10 19:15:49 -0500

    first commit

---
+++ config/plugins.ts
@@ -0,0 +1,12 @@
+// module.exports = {
+
+//   'strapi-neon-tech-db-branches': {
+//     enabled: true,
+//     config: {
+//       neonApiKey: "napi_bnmrd32tlgerv6v9zh1du2i6g7l9lssn5wt2s5x8knsnh7eh0z3sfw11a2nrsn3z", // get it from here: https://console.neon.tech/app/settings/api-keys
+//       neonProjectName: "aquacoi-data", // the neon project under wich your DB runs
+//       neonRole: "aquacoi-db_owner", // create it manually under roles for your project first
+//       gitBranch: "main" // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+//     }
+//   },
+// }
